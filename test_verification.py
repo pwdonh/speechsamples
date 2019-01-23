36993a458c7b7cfb2b002cd28ea6df45fdd6ae8b
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2019-01-23 10:12:08 -0500

    EER

--- test_verification.py
+++ test_verification.py
@@ -11,7 +11,7 @@ else:
     device = torch.device("cpu")
 
 import os
-savefile = os.environ['MEGDISK']+'/Projects/speechsamples/voxceleb_exp/exp/v1/state_dict.pkl'
+savefile = os.environ['LOCAL']+'/Projects/speechsamples/voxceleb_exp/exp/v1/state_dict.pkl'
 audio_conf = {'sample_rate': 16000, 'window_size': .025, 'window_stride': .010, 'window': 'hamming'}
 
 checkpoint_load = torch.load(savefile)
@@ -23,7 +23,7 @@ test_dataset = SpectrogramVerificationDataset(audio_conf, test_manifest)
 test_sampler = BucketingSampler(test_dataset, batch_size=1)
 test_loader = AudioDataLoader(test_dataset, num_workers=1, batch_sampler=test_sampler)
 
-# model = voxresnet34()
+model.eval()
 sim = torch.nn.CosineSimilarity()
 
 similarity = []
@@ -35,25 +35,30 @@ for i, data in enumerate(test_loader):
     out1 = model(data[0])
     out2 = model(data[1])
     similarity.append( sim(out1, out2).item() )
-    # if i==10:
-    #     break
-
-import pdb; pdb.set_trace()
 
 same = np.array(same)
 similarity = np.array(similarity)
 
-for thresh in np.arange(.5,.99,.01):
-    a=sum(((same>0)==True) & ((similarity>thresh)==False))
-    b=sum(((same>0)==False) & ((similarity>thresh)==True))
-    if a==b:
-        print(thresh)
-    print('{} {}'.format(a,b))
+diffs = []
+rates = []
+for thresh in np.arange(.5,.99,.001):
+    a=sum((same==1) & ((similarity>thresh)==False))/sum(same==1)
+    b=sum((same==0) & ((similarity>thresh)==True))/sum(same==0)
+    diffs.append(abs(a-b))
+    rates.append(np.mean([a,b]))
+    # print('{}: {} {}'.format(thresh,a,b))
+rates[np.argmin(diffs)]
 
-import matplotlib.pyplot as plt
-plt.hist(similarity[same==0])
-plt.hist(similarity[same==1])
-
+#
+# import seaborn
+# import matplotlib.pyplot as plt
+# plt.hist(similarity[same==0])
+# plt.hist(similarity[same==1])
+#
+# import pandas as pd
+# pd.DataFrame({'different': similarity[same==0], 'same': similarity[same==1]})
+#
+# tips = seaborn.load_dataset("tips")
 
 
 print('yes')

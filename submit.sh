0591063e12263a8e6302ba6812fb891961b37dca
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2019-01-23 08:22:10 -0500

    qsub submit

---
+++ submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#$ -o ./out.txt
+#$ -e ./err.txt
+#$ -m e
+
+source ~/.bashrc
+cd $LOCAL/Projects/speechsamples/voxceleb_exp/
+
+python -u train.py --cuda --savefile ./exp/v2/state_dict.pkl --model-type VoxResNetVAE > ./exp/v2/train.log
